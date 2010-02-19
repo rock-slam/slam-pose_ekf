73ed1d74a463b08f099b30d0a36a3084763cab18
Patrik Paranhos <patrik.merz_paranhos@dfki.de>
2010-02-19 15:01:18 +0100

    Changed the kalman filter to a 4 state filter with velocity measured from the odometry as input

--- src/KalmanFilter.hpp
+++ src/KalmanFilter.hpp
@@ -8,22 +8,25 @@ namespace KalmanFilter {
     class State
     {
     public:
-	static const int SIZE = 6;
+	static const int SIZE = 4;
 	typedef Eigen::Matrix<double,SIZE,1> vector_t;
 
     protected:
+      
 	vector_t _x; 
 	Eigen::Block<vector_t, 3, 1> _xi;
-	Eigen::Block<vector_t, 3, 1> _v;
+	Eigen::Block<vector_t, 1, 1> _yaw;
+	
 
     public:
 	EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 	State() :
-	    _x(vector_t::Zero()), _xi( _x.segment<3>(0) ), _v( _x.segment<3>(3) ) {};
+	    _x(vector_t::Zero()), _xi( _x.segment<3>(0) ),_yaw(_x.segment<1>(3)) {};
 
 	vector_t& vector() {return _x;};
-	Eigen::Block<vector_t, 3, 1>& xi() {return _xi;}
-	Eigen::Block<vector_t, 3, 1>& v() {return _v;}
+	Eigen::Block<vector_t, 3, 1>& xi() {return _xi;}	
+	Eigen::Block<vector_t, 1, 1>& yaw() {return _yaw;}
+	
     };
 
     class PositionKF
@@ -48,23 +51,33 @@ namespace KalmanFilter {
 	/** process noise covariance matrix */
 	Eigen::Matrix<double, State::SIZE, State::SIZE> Q;
 
-	/** observation model */
-	Eigen::Matrix<double, MEASUREMENT_SIZE, State::SIZE> H;
+	/** observation model Position*/
+	Eigen::Matrix<double, MEASUREMENT_SIZE, State::SIZE> H_pos;
+	
 	/** measurement noise covariance matrix */
-	Eigen::Matrix<double, MEASUREMENT_SIZE, MEASUREMENT_SIZE> R;
-
+	Eigen::Matrix<double, MEASUREMENT_SIZE, MEASUREMENT_SIZE> R_pos;
+	
 	/** these are just here for storage */
 	Eigen::Quaternion<double> q;
 	Eigen::Vector3d omega;
+	Eigen::Quaterniond R_n_n; 
+
+
+
 
     public:
 	EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 
-	/** update step taking acceleration in inertial frame as parameter */
-	void update( Eigen::Vector3d a );
+	/** update step taking velocity in erroneous ENU frame as parameter */
+	void update( Eigen::Vector3d v );
 
 	/** correction step, taking absolute position data */
-	void correction( Eigen::Vector3d p );
+	void correction_pos( Eigen::Vector3d p );
+	
+	/**jacobian state transition*/ 
+	Eigen::Matrix<double, State::SIZE, State::SIZE> JacobianF ( Eigen::Vector3d v );
+	
+	
     };
 }
 
