ff5ddd4b9680d24aae4f8b21988dce387e913adc
Jakob Schwendner <jakob.schwendner@dfki.de>
2010-06-23 11:47:35 +0200

    cleaned naming indentation and made most arguments const refs

--- src/EKF_PosBias_PosBias.hpp
+++ src/EKF_PosBias_PosBias.hpp
@@ -1,11 +1,9 @@
-#ifndef __EKF_POSBIAS_POSBIAS_HPP__
-#define __EKF_POSBIAS_POSBIAS_HPP__
+#ifndef __EKF_POSITIONYAWBIAS_HPP__
+#define __EKF_POSITIONYAWBIAS_HPP__
 
 #include <Eigen/Core>
 #include <Eigen/Geometry>
 #include "ExtendedKalmanFilter.hpp"
-
-
 
 namespace poseEstimation {
     class State
@@ -15,11 +13,9 @@ namespace poseEstimation {
 	typedef Eigen::Matrix<double,SIZE,1> vector_t;
 
     protected:
-      
 	vector_t _x; 
 	Eigen::Block<vector_t, 3, 1> _xi;
 	Eigen::Block<vector_t, 1, 1> _yaw;
-	
 
     public:
 	EIGEN_MAKE_ALIGNED_OPERATOR_NEW
@@ -29,18 +25,13 @@ namespace poseEstimation {
 	vector_t& vector() {return _x;};
 	Eigen::Block<vector_t, 3, 1>& xi() {return _xi;}	
 	Eigen::Block<vector_t, 1, 1>& yaw() {return _yaw;}
-	
     };
 
-    class EkfPosBias
+    class EkfPosYawBias
     {
- 	
     public:
-	
 	static const int INPUT_SIZE = 3;
 	static const int MEASUREMENT_SIZE = 4;
-
-
 
 	/** state estimate */
 	State x;
@@ -58,43 +49,34 @@ namespace poseEstimation {
 	/** measurement noise */ 
 	Eigen::Matrix<double, MEASUREMENT_SIZE, MEASUREMENT_SIZE> R; 
 	
-    
     public:
-	/** what is this line for ??? */ 
+	/** ensure alignment for eigen vector types */
 	EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 
-	EkfPosBias();
-	~EkfPosBias();
+	EkfPosYawBias();
+	~EkfPosYawBias();
 
 	/** update step taking velocity in world frame without the bias correction */
-	void update( Eigen::Vector3d v_w, double d_t );
+	void update(const Eigen::Vector3d &v_w, double d_t );
 
 	/** correction step, taking absolute position data */
-	void correctionPos(  Eigen::Matrix<double, MEASUREMENT_SIZE, 1> p );
-	
+	void correctionPos(const Eigen::Matrix<double, MEASUREMENT_SIZE, 1> &p );
 
-	/** calculates the process noise the frame should be world corrected by bias [velocity,  bias] */ 
-	void processNoise(Eigen::Matrix<double, State::SIZE, State::SIZE> Q); 
+	/** set the process noise the frame should be world corrected by bias [velocity,  bias] */ 
+	void processNoise(const Eigen::Matrix<double, State::SIZE, State::SIZE> &Q); 
 	
-	/** calculates the measurement noise in world frame [position] */ 
-	void measurementNoise(Eigen::Matrix<double, MEASUREMENT_SIZE, MEASUREMENT_SIZE> R); 
+	/** set the measurement noise in world frame [position] */ 
+	void measurementNoise(const Eigen::Matrix<double, MEASUREMENT_SIZE, MEASUREMENT_SIZE> &R); 
 
-	
-	/** configurarion hook */ 
-	void configure_hook(Eigen::Matrix<double, State::SIZE, State::SIZE> P, Eigen::Matrix<double,State::SIZE,1> x); 
-	
-	
+	/** set the inital values for state x and covariance P */
+	void init(const Eigen::Matrix<double, State::SIZE, State::SIZE> &P, const Eigen::Matrix<double,State::SIZE,1> &x); 
 	
       private: 
-	
 	/**jacobian state transition*/ 
-	Eigen::Matrix<double, State::SIZE, State::SIZE> jacobianF ( Eigen::Vector3d v_w, double d_t );
+	Eigen::Matrix<double, State::SIZE, State::SIZE> jacobianF( const Eigen::Vector3d &v_w, double d_t );
 	
 	/**jacobian observation model*/ 
-	Eigen::Matrix<double, MEASUREMENT_SIZE, State::SIZE>  jacobianH ( );	
-
-
-	
+	Eigen::Matrix<double, MEASUREMENT_SIZE, State::SIZE>  jacobianH();	
     };
 }
 
