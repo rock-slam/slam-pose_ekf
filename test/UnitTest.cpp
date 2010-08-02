77b3e518fe8a09fd8213d913b1a238c2fa16976a
Jakob Schwendner <jakob.schwendner@dfki.de>
2010-08-02 11:00:49 +0200

    moved the aggregator into drivers/aggregator/StreamAligner

--- test/UnitTest.cpp
+++ test/UnitTest.cpp
@@ -1,4 +1,3 @@
-#include "Aggregator.hpp"
 #include "KalmanFilter.hpp"
 
 #include <map>
@@ -8,38 +7,6 @@
 #include <boost/test/included/unit_test.hpp>
    
 using namespace std;
-
-template <class T> void reading_callback(base::Time ts, T data)
-{
-    cout << "ts: " << ts.seconds << ":" << ts.microseconds << " data: " << data << endl;
-}
-
-BOOST_AUTO_TEST_CASE( aggregator_test )
-{
-    aggregator::ReadingEstimator aggr;
-    aggr.setTimeout( base::Time(2,0) );
-
-    // callback, buffer_size, period_time
-    int s1 = aggr.registerStream<int>( &reading_callback<int>, 4, base::Time(1,0) ); 
-    int s2 = aggr.registerStream<string>( &reading_callback<string>, 2, base::Time(2,0) ); 
-    int s3 = aggr.registerStream<double>( &reading_callback<double> ); 
-
-    aggr.push( s1, base::Time(1), 1 );
-    aggr.push( s2, base::Time(0), string("test1") );
-    aggr.push( s2, base::Time(2), string("test2") );
-    aggr.push( s2, base::Time(4), string("test3") );
-    aggr.push( s1, base::Time(2), 2 );
-    aggr.push( s1, base::Time(2), 3 );
-    aggr.push( s1, base::Time(3), 4 );
-    aggr.push( s1, base::Time(4), 5 );
-    aggr.push( s1, base::Time(5), 6 );
-    aggr.push( s3, base::Time(5), 1.55 );
-
-    while( aggr.step() )
-    {
-	std::cout << aggr;
-    }
-}
 
 BOOST_AUTO_TEST_CASE( estimator )
 {
