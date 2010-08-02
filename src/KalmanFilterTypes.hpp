d0faf02d884a35dc8739e9905762103d9286f9d4
Patrik Paranhos <patrik.merz_paranhos@dfki.de>
2010-08-02 10:44:44 +0200

    Added Kalman Fitler Types

---
+++ src/KalmanFilterTypes.hpp
@@ -0,0 +1,32 @@
+#ifndef __KALMAN_FILTER_TYPES__
+#define __KALMAN_FILTER_TYPES__
+
+#include <base/time.h>
+#include <base/wrappers/eigen.h>
+
+namespace pose_estimator
+{ 
+
+    struct KalmanFilterState
+    {
+
+	/** the time that the slip started */
+	base::Time time;       
+
+	/** the filter state */ 
+	wrappers::Vector4 x; 
+	/** the filter covariance matrix */ 
+	wrappers::Matrix4 P;
+
+	/** the icp observation */ 
+	wrappers::Vector4 p_icp; 
+	/** the icp observation covariance */ 
+	wrappers::Matrix4 R_icp;
+	
+    };
+ 
+}
+
+#endif
+
+
