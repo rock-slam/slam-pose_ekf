f6856f728a1a3d01ab43150d8d04a95e2fb738d6
Patrik Paranhos <patrik.merz_paranhos@dfki.de>
2010-11-10 11:20:14 +0100

    new filter for CUSLAM discrete

--- src/KalmanFilter.hpp
+++ src/KalmanFilter.hpp
@@ -7,7 +7,7 @@
 
 namespace KalmanFilter {
  
-template <unsigned int SIZE, unsigned int INPUT_SIZE, unsigned int MEASUREMENT_SIZE> 
+template <unsigned int SIZE>
 class KF
     {
       
@@ -26,13 +26,33 @@ class KF
 	/** what is this line */ 
 	EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 	
+		
+	/** prediction step 
+	    F - state transition
+	    Q - process noise covariance matrix, 
+	    dt - discrete time time step 
+	    */
+	void predictionDiscrete(Eigen::Matrix<double, SIZE, SIZE> F,
+	      Eigen::Matrix<double, SIZE, SIZE> Q,
+	      double dt)
+	{
+
+		//State transition 
+		x = ( Eigen::Matrix<double, SIZE, SIZE>::Identity() + F * dt + F * dt * dt / 2 ) * x; 
+		
+		//covariance update 
+		P = P + (F * P + P * F.transpose()) * dt + F * P * F.transpose() * dt * dt + Q * dt; 
+		
+	}
+ 
 	/** update step 
 	    u - Input 
 	    B - relation between input and state 
 	    F - state transition
 	    Q - process noise covariance matrix, 
 	    */
-	void update(Eigen::Matrix<double, INPUT_SIZE, 1> u,
+	template<unsigned int INPUT_SIZE> 
+	void prediction(Eigen::Matrix<double, INPUT_SIZE, 1> u,
 	      Eigen::Matrix<double,SIZE ,INPUT_SIZE> B, 
 	      Eigen::Matrix<double, SIZE, SIZE> F,
 	      Eigen::Matrix<double, SIZE, SIZE> Q )
@@ -45,32 +65,89 @@ class KF
 		P = F*P*F.transpose() + Q;
 		
 	}
+	
  
 	/** correction step, 
 	 p - observation 
 	 H - observation function
 	 R - measurement noise covariance matrix
 	*/	
+	template<unsigned int MEASUREMENT_SIZE> 
 	void correction( Eigen::Matrix<double, MEASUREMENT_SIZE, 1> p,
 	  Eigen::Matrix<double, MEASUREMENT_SIZE, SIZE> H,
 	  Eigen::Matrix<double, MEASUREMENT_SIZE, MEASUREMENT_SIZE> R)	  
 	{
-	    // correct the estimate and covariance according to measurement
-	    
+	    // innovation  
 	    Eigen::Matrix<double, MEASUREMENT_SIZE, 1> y = p - H*x;
-	    
+	    //innovation covariance
 	    Eigen::Matrix<double, MEASUREMENT_SIZE, MEASUREMENT_SIZE> S = H*P*H.transpose()+R;
-
+	    //gain 
 	    Eigen::Matrix<double, SIZE, MEASUREMENT_SIZE> K = P*H.transpose()*S.inverse();
+	    //update
+	    x = x + K*y;
+	    
+	    P = (Eigen::Matrix<double, SIZE, SIZE>::Identity() - K*H)*P;
+	    
+	    
+	}
+
+
+	template <unsigned int MEASUREMENT_SIZE>
+	Eigen::Matrix<double, MEASUREMENT_SIZE, 1>  innovation( Eigen::Matrix<double, MEASUREMENT_SIZE, 1> p,
+	   Eigen::Matrix<double, MEASUREMENT_SIZE, SIZE> H)	  
+	{
+	    
+	    return (p - H*x);
+	    
+	}
+	
+ 	/** innovation covariance step, 
+	 p - observation 
+	 h - observation model function
+	 H -  observation model
+	 R - measurement noise covariance matrix
+	*/	
+	template <unsigned int MEASUREMENT_SIZE>
+	Eigen::Matrix<double, MEASUREMENT_SIZE, MEASUREMENT_SIZE>  innovationCovariance(
+	  Eigen::Matrix<double, MEASUREMENT_SIZE, SIZE> H,
+	  Eigen::Matrix<double, MEASUREMENT_SIZE, MEASUREMENT_SIZE> R)	  
+	{
 	  
+	    return (H*P*H.transpose()+R);
+  
+	}
+	
+	/** update step, 
+	 H - observation model
+	 y - innovation
+	 K - gain 
+	*/	
+	template <unsigned int MEASUREMENT_SIZE>
+	void update( Eigen::Matrix<double, MEASUREMENT_SIZE, SIZE> H,
+		     Eigen::Matrix<double, SIZE, MEASUREMENT_SIZE> K,
+		     Eigen::Matrix<double, MEASUREMENT_SIZE, 1> y)	  
+	{
+
 	    x = x + K*y;
 	    
 	    P = (Eigen::Matrix<double, SIZE, SIZE>::Identity() - K*H)*P;
 	    
 	}
 
+	/** calculates the gain 
+	 J_H - jacobian observation model
+	*/
+	template <unsigned int MEASUREMENT_SIZE>
+	Eigen::Matrix<double, SIZE, MEASUREMENT_SIZE> gain( 
+	  Eigen::Matrix<double, MEASUREMENT_SIZE, SIZE> H,
+	  Eigen::Matrix<double, MEASUREMENT_SIZE, MEASUREMENT_SIZE> S )	  
+	{
 
-	
+	    // correct the estimate and covariance according to measurement
+	    return (P*H.transpose()*S.inverse());
+	    
+	}
+
     };
 }
 
