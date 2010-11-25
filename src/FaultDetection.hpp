07afbc6f89bff98876488ace3481a31ca987a3d7
Jakob Schwendner <jakob.schwendner@dfki.de>
2010-11-25 17:56:03 +0100

    moved FaultDetection.hpp from fault_detection package

---
+++ src/FaultDetection.hpp
@@ -0,0 +1,35 @@
+
+#ifndef FaultDetection_HPP
+#define FaultDetection_HPP
+#include <Eigen/Core>
+#include <Eigen/Geometry>
+
+namespace fault_detection{ 
+
+
+class ChiSquared 
+{
+    public: 
+	
+    /**
+    Uses the Chi_Square distribution under gaussion assumptions to reject or not an innovation 
+    */ 
+    template <unsigned int DEGREE_OF_FREEDOM>
+    bool rejectData(Eigen::Matrix< double, DEGREE_OF_FREEDOM, 1 > innovation,
+		    Eigen::Matrix< double, DEGREE_OF_FREEDOM, DEGREE_OF_FREEDOM > innovation_covariance, 
+		    float threshold)
+    {
+
+      
+	  if ( (innovation.transpose() * innovation_covariance * innovation)[0] <= threshold )  
+	      return false;
+	  else
+	      return true; 
+	  
+	  
+    }	    
+
+};
+  
+}
+#endif 
